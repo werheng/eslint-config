ddeeb6de4141418d75ca631a34151942c44fd98e
Werheng <vhang97@163.com>
2021-08-20 15:50:51 +0800

    feat(basic): add rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -5,4 +5,10 @@ module.exports = {
     node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    // Common
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2, { SwitchCase: 1, outerIIFEBody: 1 }],
+  },
 }
