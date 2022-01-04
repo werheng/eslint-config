0415c8e466684bcbc6e7e199267c8bd01fe2598e
Werheng <vhang97@163.com>
2022-01-04 15:04:42 +0800

    feat(ts): add rules

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,4 +3,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     '@werheng/eslint-config-basic',
   ],
+  rules: {
+    // off
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
 }
