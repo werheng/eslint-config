29a1367e0efafcbc7daa1c9860e8cbd90d441f57
Werheng <vhang97@163.com>
2022-01-04 15:26:30 +0800

    fix(basic): sort rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -7,8 +7,8 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     // Common
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
     indent: ['error', 2, { SwitchCase: 1, outerIIFEBody: 1 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
   },
 }
