2c058826bc3c7e7205463388f5a5062cc371b39a
Werheng <vhang97@163.com>
2022-01-04 15:13:43 +0800

    feat(ts): add rules

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -7,5 +7,6 @@ module.exports = {
     // off
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
